5ceef417fb185a16858fe9ad1bfea17b7797420d
SammeJandeson <samme.rodrigues@quintoandar.com.br>
2022-10-18 16:01:30 -0300

    updated REPL

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"monkey/lexer"
+	"monkey/parser"
 	"monkey/token"
 )
 
@@ -21,8 +22,17 @@ func Start(in io.Reader, out io.Writer) {
         }
 
         line := scanner.Text()
-
         l := lexer.New(line)
+        p := parser.New(l)
+
+        program := p.ParseProgram()
+        if len(p.Errors()) != 0 {
+            printParserErrors(out, p.Errors())
+            continue
+        }
+
+        io.WriteString(out, program.String())
+        io.WriteString(out, "\n")
 
         for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
             fmt.Printf("%+v\n", tok)
@@ -30,3 +40,8 @@ func Start(in io.Reader, out io.Writer) {
     }
 }
 
+func  printParserErrors(out io.Writer, errors []string) {
+    for _, msg := range errors {
+        io.WriteString(out, "\t"+msg+"\t")
+    }
+}
