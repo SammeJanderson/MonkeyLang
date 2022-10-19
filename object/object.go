d48d400da652a87864de21a7c9148b728bc783fd
SammeJandeson <samme.rodrigues@quintoandar.com.br>
2022-10-19 16:24:55 -0300

    object representation

---
+++ object/object.go
@@ -0,0 +1,47 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const (
+    INTEGER_OBJ = "INTEGER"
+    STRING_OBJ = "STRING"
+    BOOLEAN_OBJ = "BOOLEAN"
+    NULL_OBJ = "NULL"
+)
+
+type Object interface {
+    Type() ObjectType
+    Inspect() string
+}
+
+type Integer struct {
+    Value int64
+}
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value)}
+
+
+type String struct{
+    Value string
+}
+
+func (s *String) Type() ObjectType { return STRING_OBJ }
+func (s *String) Inspect() string { return fmt.Sprintf("%s", s.Value)}
+
+
+type Boolean struct {
+    Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value)}
+
+
+type Null struct {}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string { return "null" }
+
