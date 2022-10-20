4d36c337259fdbb3b64b8f15dcb99c640adba36d
SammeJandeson <samme.rodrigues@quintoandar.com.br>
2022-10-20 16:46:09 -0300

    better boolean eval

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
 	"monkey/object"
 )
 
+var (
+    TRUE = &object.Boolean{Value: true}
+    FALSE = &object.Boolean{Value: false}
+)
+
 
 func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
@@ -21,7 +26,6 @@ func Eval(node ast.Node) object.Object {
         return &object.Boolean{Value: node.Value}
     }
 
-
     return nil
 }
 
@@ -34,4 +38,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
     }
 
     return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+    if input {
+        return TRUE
+    }
+    return FALSE
 }
