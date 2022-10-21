3cb628cb03078966530b4ca60af96faac564745e
SammeJandeson <samme.rodrigues@quintoandar.com.br>
2022-10-21 11:26:25 -0300

    fixed boolean eval

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -3,6 +3,7 @@ package evaluator
 import (
 	"monkey/ast"
 	"monkey/object"
+    "monkey/token"
 )
 
 var (
@@ -24,15 +25,37 @@ func Eval(node ast.Node) object.Object {
         return &object.Integer{Value: node.Value}
 
     case *ast.Boolean:
-        return &object.Boolean{Value: node.Value}
+        return nativeBoolToBooleanObject(node.Value)
 
-    case nil:
-        return NULL
+    case *ast.PrefixExpression:
+        right := Eval(node.Right)
+        return evalPrefixExpression(node.Operator, right)
     }
 
     return nil
 }
 
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+    switch operator {
+        case  token.BANG:
+            return evalBangOperatorExpression(right)
+        default:
+            return NULL
+    }
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+switch right {
+    case TRUE:
+        return FALSE
+    case FALSE:
+return TRUE
+    case NULL:
+        return TRUE
+    default:
+        return FALSE
+    }
+}
 
 func evalStatements(stmts []ast.Statement) object.Object {
     var result object.Object
