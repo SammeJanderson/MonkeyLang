0a1742a784cd00c87c3910b395748ea66065cbd0
SammeJandeson <samme.rodrigues@quintoandar.com.br>
2022-10-21 16:58:30 -0300

    if else expression eval

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -28,6 +28,31 @@ func TestBangOperator(t *testing.T) {
 
 }
 
+func TestIfElseExpression(t *testing.T) {
+    tests := []struct {
+        input string
+        expected
+    }{
+        {"if (true) { 10 }", 10},
+        {"if (false) { 10 }", nil},
+        {"if (1) { 10 }", 10},
+        {"if (1 < 2) { 10 }", 10},
+        {"if (1 > 2) { 10 }", nil},
+        {"if (1 < 2) { 10 } else { 20 }", 20},
+        {"if (1 < 2) { 10 }", 10},
+    }
+     for _, tt := range tests {
+        evaluated := testEval(tt.input)
+        integer, ok := tt.expected.(int)
+        if ok {
+            testIntegerObject(t, evaluated, int64(integer))
+        } else {
+            testNullObject(t, evaluated)
+        }
+    }
+
+}
+
 func TestEvalBooleanExpression(t *testing.T) {
     tests := []struct {
         input string
@@ -130,3 +155,10 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
     return true
 }
 
+func testNullObject(t *testing.T, obj object.Object) bool {
+    if obj != NULL {
+        t.Errorf("object is not NULL. got=%T (%+v)", obj, obj)
+        return false
+    }
+    return true
+}
