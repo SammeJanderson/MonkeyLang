92e3abf35e3a4e0ce5d857754a05157079a419e5
SammeJandeson <samme.rodrigues@quintoandar.com.br>
2022-10-25 16:59:00 -0300

    function literal eval

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -77,8 +77,11 @@ func Eval(node ast.Node, env *object.Enviroment) object.Object {
 
         env.Set(node.Name.Value, val)
 
-    
-    
+    case *ast.FunctionLiteral:
+        params := node.Parameters
+        body := node.Body
+        return &object.Function{Parameters: params, Env: env, Body: body}
+
     }
 
     return nil
