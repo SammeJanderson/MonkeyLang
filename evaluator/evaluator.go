56333ffaa26090110d286b98b3cb6f9452c57edf
SammeJandeson <samme.rodrigues@quintoandar.com.br>
2022-10-25 13:07:16 -0300

    added error branch to eval

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -30,11 +30,24 @@ func Eval(node ast.Node) object.Object {
 
     case *ast.PrefixExpression:
         right := Eval(node.Right)
+
+        if isError(right) {
+            return right
+        }
+    
         return evalPrefixExpression(node.Operator, right)
 
     case *ast.InfixExpression:
         left := Eval(node.Left)
+        if isError(left) {
+            return left
+        }
+
         right := Eval(node.Right)
+        if isError(right) {
+            return right
+        }
+
         return evalInfixExpression(node.Operator, left, right)
 
     case *ast.BlockStatement:
@@ -45,6 +58,11 @@ func Eval(node ast.Node) object.Object {
 
     case *ast.ReturnStatement:
         val := Eval(node.ReturnValue)
+
+        if isError(val){
+        return val
+        }
+
         return &object.ReturnValue{Value: val}
     }
 
@@ -58,6 +76,9 @@ func newError(format string, a ...interface{}) *object.Error{
 
 func evalIfExpression(ie *ast.IfExpression) object.Object {
     condition := Eval(ie.Condition)
+    if isError(condition) {
+        return condition
+    }
 
     if isTruthy(condition) {
         return Eval(ie.Consequence)
@@ -201,4 +222,10 @@ func evalIntegerInfixExpression(
     }
 }
 
+func isError(obj object.Object) bool {
+    if obj != nil {
+        return obj.Type() == object.ERROR_OBJ
+    }
+    return false
+}
 
